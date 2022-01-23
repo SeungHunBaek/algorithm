683db51e371dda53fda71b1d1ce68852ceba913b
baek <xemeal@gmail.com>
2022-01-23 17:47:49 +0900

    case13

--- session_1/case_13.js
+++ session_1/case_13.js
@@ -1,6 +1,17 @@
 function solution(str){
-    let answer = str.toUpperCase();
+    let answer = '';
+
+    for (const value of str) {
+        let asc = value.charCodeAt();
+        // 대문자인 경우
+        if(asc >= 65 && asc <= 90) {
+            answer += value.toLowerCase();
+        // 소문자인 경우
+        } else if(asc >= 97 && asc <= 122) {
+            answer += value.toUpperCase();
+        }
+    }
     return answer;
 }
 
-console.log(solution('ItisTimeToStudy')) // ITISTIMETOSTUDY
+console.log(solution('StuDY')) // sTUdy
