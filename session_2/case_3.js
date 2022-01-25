02f3c5474e7a2896d87798e41c742fb1967c6881
baek <xemeal@gmail.com>
2022-01-25 19:32:28 +0900

    case3

--- session_2/case_3.js
+++ session_2/case_3.js
@@ -1,7 +1,14 @@
-function solution(n){
-    let answer;
-
+function solution(A, B){
+    let answer = [];
+    for (let i = 0; i < A.length; i++) {
+        if(A[i] - B[i] === 1 || A[i] - B[i] === -2) answer.push('A');
+        else if(A[i] - B[i] === -1 || A[i] - B[i] === 2) answer.push('B');
+        else answer.push('D');
+    }
     return answer;
 }
 
-console.log(solution())
+const A = [2,3,3,1,3];
+const B = [1,1,2,2,3];
+ 
+console.log(...solution(A, B));
