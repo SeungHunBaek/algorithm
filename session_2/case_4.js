941c9251b7eca68bd774bd39de0c204fa2f5daac
baek <xemeal@gmail.com>
2022-01-25 19:54:57 +0900

    case4

--- session_2/case_4.js
+++ session_2/case_4.js
@@ -1,7 +1,14 @@
 function solution(n){
-    let answer;
-
+    let answer=0, cnt=0;
+    for (let i = 0; i < n.length; i++) {
+        if(n[i]===1){
+            cnt++;
+        } else {
+            cnt=0
+        }
+        answer+=cnt;
+    }
     return answer;
 }
 
-console.log(solution())
+console.log(solution([1,0,1,1,1,0,0,1,1,0]));
