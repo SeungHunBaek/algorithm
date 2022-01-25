b6605284d5ec9d88b8a2482c9f6221230d9e2c6d
baek <xemeal@gmail.com>
2022-01-25 19:15:49 +0900

    case2

--- session_2/case_2.js
+++ session_2/case_2.js
@@ -1,7 +1,14 @@
 function solution(n){
-    let answer;
+    let answer = 0, max = Number.MIN_SAFE_INTEGER;
 
+    n.forEach(element => {
+        if(element > max) {
+            max = element;
+            answer++;
+        }
+    });
     return answer;
 }
 
-console.log(solution())
+let arr=[130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution(arr)); // 5
