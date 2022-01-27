0277b781aff6777da3ebbd47c9ab20c8bcf0cef8
baek <xemeal@gmail.com>
2022-01-27 20:12:01 +0900

    case6

--- session_2/case_6.js
+++ session_2/case_6.js
@@ -1,7 +1,30 @@
 function solution(n){
-    let answer;
+    let answer=sum1=sum2=0;
+    for (let i = 0; i < n.length; i++) {
+        sum1=sum2=0;
+        for (let j = 0; j < n.length; j++) {
+            sum1 += n[i][j];
+            sum2 += n[j][i];
+        }
+        answer = Math.max(answer, sum1, sum2);
+    }
 
+    sum1=sum2=0;
+
+    for (let i = 0; i < n.length; i++) {
+        sum1 += n[i][i];
+        sum2 += n[i][n.length-1-i];
+    }
+
+    answer = Math.max(answer, sum1, sum2);
     return answer;
 }
 
-console.log(solution())
+let arr=[
+         [10, 13, 10, 12, 15], 
+         [12, 39, 30, 23, 11],
+         [11, 25, 50, 53, 15],
+         [19, 27, 29, 37, 27],
+         [19, 13, 30, 13, 19]
+        ];
+console.log(solution(arr)) // 155
