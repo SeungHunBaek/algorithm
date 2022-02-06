5396f1b06d007ba76efa76edb5b93b9294a03ac1
baek <xemeal@gmail.com>
2022-02-06 21:09:31 +0900

    case4

--- session_3/case_4.js
+++ session_3/case_4.js
@@ -1,40 +1,26 @@
 function solution(str, target){
     let answer = [];
-    let splitStr = str.split('');
-    let targetIndex = splitStr.indexOf(target);
-    splitStr.forEach((element, index) => {
-
-        let distance = Math.abs(index - targetIndex);
-        answer.push(distance);
-    });
+    let standard = 1000;
+    for (const value of str) {
+        if(value === target) {
+            standard = 0;
+            answer.push(standard);
+        } else {
+            standard++;
+            answer.push(standard);
+        }
+    }
+    standard = 1000;
+    for (let i = str.length-1; i >= 0; i--) {
+        if(str[i] === target){
+            standard = 0;
+        } else if(answer[i] > standard){
+            standard++;
+            answer[i] = standard;
+        }        
+    }
     
     return answer;
 }
-
-
-function solution2(s, t){
-    let answer=[];
-    let p=1000;
-    for(let x of s){
-        if(x===t){
-            p=0;
-            answer.push(p);
-        }
-        else{
-            p++;
-            answer.push(p);
-        }
-    }
-    p=1000;
-    for(let i=s.length-1; i>=0; i--){
-        if(s[i]===t) p=0;
-        else{
-            p++;
-            answer[i]=Math.min(answer[i], p);
-        }
-    }
-    return answer;
-}
 let str="teachermode";
-console.log(solution(str, 'e')); // 10121012210
-console.log(solution2(str, 'e'));
+console.log(solution(str, 'e').join('') === '10121012210'); // 10121012210
