678359d6c0f7570c702b1c1cea5db6943a6aa9f0
baek <xemeal@gmail.com>
2022-02-07 20:00:06 +0900

    case1

--- session_4/case_1.js
+++ session_4/case_1.js
@@ -1,7 +1,40 @@
 function solution(n){
-    let answer;
+    let answer=0, max=0;
 
+    n.forEach(value => {
+        let sum = 0;
+        for (let i = 0; i < String(value).length; i++) {
+            sum+= parseInt(String(value)[i]);
+        }
+        
+        if(max <= sum) {
+            if(answer < value){
+                max = sum;
+                answer = value;
+            }
+        }
+        sum = 0;
+    });
     return answer;
 }
-
-console.log(solution())
+function solution2(arr){
+    let answer, max=Number.MIN_SAFE_INTEGER;
+    for(let x of arr){
+        let sum=0, tmp=x;
+        while(tmp){
+            sum+=(tmp%10);
+            tmp=Math.floor(tmp/10);
+        }
+        if(sum>max){
+            max=sum;
+            answer=x;
+        }
+        else if(sum===max){
+            if(x>answer) answer=x;
+        }
+    }
+    return answer;
+}
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(arr))
+console.log(solution2(arr))
