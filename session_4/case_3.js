647191adf278e62f130f53cd41e5c340f92f9f0f
baek <xemeal@gmail.com>
2022-02-08 23:15:22 +0900

    case3

--- session_4/case_3.js
+++ session_4/case_3.js
@@ -1,7 +1,28 @@
-function solution(n){
-    let answer;
-
+function solution(test){
+    let answer =[];
+    let m = test.length;
+    let n = test[0].length;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            let cnt=0
+            for (let k = 0; k < m; k++) {
+                let pi=pj=0;
+                for (let s = 0; s < n; s++) {
+                    if(test[k][s] === i) pi=s;
+                    if(test[k][s] === j) pj=s;
+                }
+                if(pi < pj) cnt++;
+            }
+            if(cnt === m) answer.push([i,j]);
+         }
+    }
+    
     return answer;
 }
 
-console.log(solution())
+let arr=[
+          [3, 4, 1, 2],
+          [4, 3, 2, 1],
+          [3, 1, 4, 2]
+        ];
+console.log(solution(arr)); // (3, 1), (3, 2), (4, 2)
