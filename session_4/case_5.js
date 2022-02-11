a482569cb47a07156bc14fcb8573520083a8e067
baek <xemeal@gmail.com>
2022-02-11 16:39:44 +0900

    case5

--- session_4/case_5.js
+++ session_4/case_5.js
@@ -1,7 +1,20 @@
-function solution(n){
-    let answer;
-
+function solution(n, array){
+    let answer, tempArr = [];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if(i === j) continue;
+            for (let k = 0; k < array.length; k++) {
+                if(j === k || k ===i) continue;
+                tempArr.push(arr[i] + arr[j] + arr[k]);
+            }
+        }
+    }
+    tempArr = new Set(tempArr);
+    answer = [...tempArr].sort((a, b) => {
+        return b-a;
+    })[n-1];
+    
     return answer;
 }
-
-console.log(solution())
+let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(3, arr)); // 143
