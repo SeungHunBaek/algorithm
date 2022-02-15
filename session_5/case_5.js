abfd90632e9d0f286b57ebf7c61749744f8fca1b
baek <xemeal@gmail.com>
2022-02-15 22:27:52 +0900

    case5

--- session_5/case_5.js
+++ session_5/case_5.js
@@ -11,5 +11,20 @@ function solution(k, array){
 
     return answer;
 }
+function solution2(k, array) {
+    let answer = 0;
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum+=array[i];
+    }
+    answer = sum;
+    for (let j = k; j < array.length; j++) {
+        sum+=(array[j]-array[j-k]);
+        answer = Math.max(answer, sum);
+    }
+
+    return answer;
+}
 let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
 console.log(solution(3, a)); // 56
+console.log(solution2(3, a)); // 56
