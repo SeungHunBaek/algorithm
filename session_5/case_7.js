c802344ac6a04cdb557e650a60336b965eef8029
baek <xemeal@gmail.com>
2022-02-16 20:30:51 +0900

    case7

--- session_5/case_7.js
+++ session_5/case_7.js
@@ -30,8 +30,27 @@ function solution2(str1, str2){
     }
 
     for (const value of str2) {
-        if(strHM1.has(value) && strHM1.get(value) >= 0) strHM1.set(value, strHM1.get(value)-1);
+        if(strHM1.has(value) && strHM1.get(value) !== 0) strHM1.set(value, strHM1.get(value)-1);
         else answer = false;
+    }
+
+    return answer;
+}
+function solution3(str1, str2){
+    let answer = true;
+    let strHM1 = new Map();
+    let strHM2 = new Map();
+    for (const value of str1) {
+        if(strHM1.has(value)) strHM1.set(value, strHM1.get(value)+1);
+        else strHM1.set(value, 1);
+    }
+    for (const value of str2) {
+        if(strHM2.has(value)) strHM2.set(value, strHM2.get(value)+1);
+        else strHM2.set(value, 1);
+    }
+    for (const [key, value] of strHM1) {
+        // console.log(key, value, strHM2.get(key))
+        if(!strHM2.has(key) || value !== strHM2.get(key)) return false
     }
 
     return answer;
@@ -41,3 +60,4 @@ let a="AbaAeCe";
 let b="baeeACA";
 console.log(solution(a, b)); // true
 console.log(solution2(a, b)); // true
+console.log(solution3(a, b)); // true
