197ed5f7122ae370834fba27b4eaa2ea4c1f97f3
baek <xemeal@gmail.com>
2022-02-16 19:26:56 +0900

    case7

--- session_5/case_7.js
+++ session_5/case_7.js
@@ -11,7 +11,7 @@ function solution(str1, str2){
         else strHM2.set(value, 1);
     }
     for (const [key, value] of strHM1) {
-        console.log(key, value, strHM2.get(key))
+        // console.log(key, value, strHM2.get(key))
         if(value !== strHM2.get(key)) {
             answer = false;
             break;
@@ -20,7 +20,24 @@ function solution(str1, str2){
 
     return answer;
 }
+function solution2(str1, str2){
+    let answer = true;
+    let strHM1 = new Map();
+
+    for (const value of str1) {
+        if(strHM1.has(value)) strHM1.set(value, strHM1.get(value)+1);
+        else strHM1.set(value, 1);
+    }
+
+    for (const value of str2) {
+        if(strHM1.has(value) && strHM1.get(value) >= 0) strHM1.set(value, strHM1.get(value)-1);
+        else answer = false;
+    }
+
+    return answer;
+}
 
 let a="AbaAeCe";
 let b="baeeACA";
 console.log(solution(a, b)); // true
+console.log(solution2(a, b)); // true
