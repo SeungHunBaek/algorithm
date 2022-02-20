7a39ddca049980cf04cc62c64e7e7d1ed3c38330
baek <xemeal@gmail.com>
2022-02-20 17:58:59 +0900

    case4

--- session_6/case_4.js
+++ session_6/case_4.js
@@ -1,7 +1,21 @@
-function solution(n){
-    let answer;
-
+function solution(str){
+    let answer = 0;
+    let stack = [];
+    for (const value of str) {
+        if(isNaN(value)) {
+            let right = stack.pop(); 
+            let left = stack.pop();
+            if(value === '+') stack.push(left + right);
+            else if(value === '-') stack.push(left - right);
+            else if(value === '*') stack.push(left * right);
+            else if(value === '/') stack.push(left / right);
+        } else {
+            stack.push(parseInt(value));
+        }
+    }
+    answer = stack[0];
     return answer;
 }
 
-console.log(solution())
+let str="352+*9-";
+console.log(solution(str)); // 12
