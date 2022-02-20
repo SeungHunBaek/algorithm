46b6261965cfd441eb3f3bd7e9df24310e3e458d
baek <xemeal@gmail.com>
2022-02-20 15:25:21 +0900

    case2

--- session_6/case_2.js
+++ session_6/case_2.js
@@ -1,7 +1,15 @@
-function solution(n){
+function solution(str){
     let answer;
-
+    let strArray = [];
+    for (const value of str) {
+        if(value !== ')') strArray.push(value);
+        else if(value === ')') {
+            while(strArray.pop() !== '(');
+        };
+    }
+    answer = strArray.join('');
     return answer;
 }
 
-console.log(solution())
+let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+console.log(solution(str)); // EFLM
