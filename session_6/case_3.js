8ef3ef3bbd8dae185aec173c0f3989c0311190ae
baek <xemeal@gmail.com>
2022-02-20 16:49:12 +0900

    case3

--- session_6/case_3.js
+++ session_6/case_3.js
@@ -18,7 +18,26 @@ function solution(board, moves){
     }
     return answer;
 }
-
+function solution2(board, moves){
+    let answer=0;
+    let stack=[];
+    moves.forEach(pos => {
+        for(let i=0; i<board.length; i++){
+            if(board[i][pos-1]!==0){
+                let tmp=board[i][pos-1];
+                board[i][pos-1]=0;
+                if(tmp===stack[stack.length-1]){
+                    stack.pop();
+                    answer+=2;
+                }
+                else stack.push(tmp);
+                break;
+            }
+        }
+    });
+                    
+    return answer;
+}
 let board=[
         [0,0,0,0,0],
         [0,0,1,0,3],
@@ -30,3 +49,13 @@ let board=[
 let moves=[1, 5, 3, 5, 1, 2, 1, 4];
 // 4 3 1 1 3 2 
 console.log(solution(board, moves)); // 4
+
+let board2=[
+    [0,0,0,0,0],
+    [0,0,1,0,3],
+    [0,2,5,0,1],
+    [4,2,4,4,2],
+    [3,5,1,3,1]
+];
+let moves2=[1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution2(board2, moves2)); // 4
