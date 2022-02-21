d01931c23917f74e1530c8b046b78fdb76695182
baek <xemeal@gmail.com>
2022-02-22 08:27:43 +0900

    case5

--- session_6/case_5.js
+++ session_6/case_5.js
@@ -1,4 +1,18 @@
-function solution(n){
+function solution(str){
+    let answer=0;
+    let stack=[];
+    for (let i = 0; i < str.length; i++) {
+        if(str[i] === '(') stack.push(str[i]);
+        else {
+            stack.pop();
+            if(str[i-1]  === '(') answer += stack.length;
+            else answer++;
+        }
+    }
+    return answer;
+}
+
+function solution2(s){
     let answer=0;
     let stack=[];
     for(let i=0; i<s.length; i++){
@@ -13,4 +27,5 @@ function solution(n){
 }
 
 let a="()(((()())(())()))(())";
-console.log(solution(a));
+console.log(solution(a)); // 17
+console.log(solution2(a)); // 17
