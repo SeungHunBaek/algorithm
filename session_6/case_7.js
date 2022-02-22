7ed6247bff5ed0dc51f61985645eebc740f86d3b
baek <xemeal@gmail.com>
2022-02-22 21:47:08 +0900

    case7

--- session_6/case_7.js
+++ session_6/case_7.js
@@ -1,7 +1,15 @@
-function solution(n){
-    let answer;
+function solution(a,b){
+    let answer = false;
+    let queue = a.split('');
+    
+    for (const value of b) {
+        if(queue.indexOf(value) > -1 && queue[0] === value) queue.shift();
+    }
+    if(queue.length === 0) answer = true;
 
     return answer;
 }
 
-console.log(solution())
+let a="CBA";
+let b="CBDGEA";
+console.log(solution(a, b)); //true
