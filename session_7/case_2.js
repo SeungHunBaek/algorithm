5eb90cea330014c994e925f6de4fecf34abde7a1
baek <xemeal@gmail.com>
2022-02-23 12:42:12 +0900

    case2

--- session_7/case_2.js
+++ session_7/case_2.js
@@ -1,7 +1,15 @@
-function solution(n){
-    let answer;
+function solution(array){
+    let answer = array;
+    for (let i = 0; i < array.length-1; i++) {
+        for (let j = 0; j < array.length-i-1; j++) {
+            if(array[j] > array[j+1]) {
+                [array[j], array[j+1]] = [array[j+1], array[j]];
+            }
+        }
+    }
 
     return answer;
 }
 
-console.log(solution())
+let arr=[13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
