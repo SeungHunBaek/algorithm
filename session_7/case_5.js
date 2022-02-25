6b46479e29422f8d46b30a74b92f39e6e3f2bba7
baek <xemeal@gmail.com>
2022-02-25 21:53:06 +0900

    case5

--- session_7/case_5.js
+++ session_7/case_5.js
@@ -18,6 +18,29 @@ function solution(n, k){
     answer = cache;
     return answer;
 }
+function solution2(n, k){
+    let answer;
+    let cache = Array.from({length:n}, (value, index) => {
+        return 0;
+    })
+
+    k.forEach((element, index) => {
+        // hit
+        if(cache.includes(element)) {
+            cache.splice(cache.indexOf(element), 1);
+            cache.unshift(element);  
+        // miss
+        } else {
+            cache.unshift(element);
+            if(cache.length > n) {
+                cache.pop();
+            }
+        }
+    });
+    answer = cache;
+    return answer;
+}
 
 let arr=[1, 2, 3, 2, 6, 2, 3, 5, 7];
 console.log(`answer: ${solution(5, arr)}`); // 75326
+console.log(`answer: ${solution2(5, arr)}`); // 75326
