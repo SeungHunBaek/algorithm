431e4d6041b7cc06ac3e2d535e074f06dfb87600
baek <xemeal@gmail.com>
2022-03-16 23:08:41 +0900

    case2

--- session_9/case_2.js
+++ session_9/case_2.js
@@ -1,8 +1,31 @@
-function solution(array){
-    let answer = array;
-    
+function solution(n, array){
+    let answer = 0;
+    let graph = Array.from(Array(n+1), () => Array(n+1).fill(0));
+    let checkArray = Array.from({length:n+1}, () => 0);
+    // 방향그래프 작성
+    for (const [a, b] of array) {
+        graph[a][b] = 1;
+    }
+
+    function DFS(v){
+        if(v === n) {
+            answer++;
+        } else {
+            for (let i = 1; i <= n; i++) {
+                if(graph[v][i] === 1 && checkArray[i] === 0){
+                    checkArray[i] = 1;
+                    DFS(i);
+                    checkArray[i] = 0;
+                }
+                
+            }
+        }
+    }
+    checkArray[1] = 1;
+    DFS(1);
     return answer;
 }
 
-let arr=[];
-console.log(solution(arr));
+// 간선 정보
+let arr=[[1, 2], [1, 3], [1, 4], [2, 1], [2, 3], [2, 5], [3, 4], [4, 2], [4, 5]];
+console.log(solution(5, arr));
