bbd3ce1141ccc6ef066b3e1a45039e4fc2d23734
baek <xemeal@gmail.com>
2022-03-18 14:50:42 +0900

    case5

--- session_9/case_5.js
+++ session_9/case_5.js
@@ -1,8 +1,20 @@
-function solution(array){
-    let answer = array;
+function solution(){
+    let answer = [];
+    let queue =[];
+    queue.push(1);
+
+    while(queue.length){
+        let v = queue.shift();
+        answer += v +" ";
+        for (const nv of [v*2, v*2+1]) {
+            if(nv >7 ) continue;
+            else {
+                queue.push(nv);
+            }
+        }
+    }
     
     return answer;
 }
 
-let arr=[];
-console.log(solution(arr));
+console.log(solution());
