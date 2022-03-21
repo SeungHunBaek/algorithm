ff0d4cd2d8147b66a2e90d07a0c1eaa5553f1a17
baek <xemeal@gmail.com>
2022-03-21 21:14:56 +0900

    case3

--- session_10/case_3.js
+++ session_10/case_3.js
@@ -1,8 +1,21 @@
 function solution(array){
-    let answer = array;
+    let answer = 0;
+    let dynamicTable = Array.from({length: array.length}, () => 0);
+
+    dynamicTable[0] = 1;
+    for (let i = 1; i < array.length; i++) {
+        let max = 0;
+        for (let j = i-1; j >= 0; j--) {
+            if(array[i] > array[j] && max < dynamicTable[j]){
+                max = dynamicTable[j];
+            }
+        }
+        dynamicTable[i] = max + 1;
+        answer = Math.max(answer, dynamicTable[i]);
+    }
     
     return answer;
 }
 
-let arr=[];
+let arr=[5, 3, 7, 8, 6, 2, 9, 4];
 console.log(solution(arr));
